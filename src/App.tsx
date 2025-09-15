b64e890b03a7eb0b0a0150086c79dfdb64718368
olhkyle <itskkyle@gmail.com>
2025-09-15 15:32:24 +0900

    rule : change Declarative Function into Arrow Function

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 
-function App() {
+const App = () => {
 	const [count, setCount] = useState(0);
 
 	return (
@@ -25,6 +25,6 @@ function App() {
 			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
 		</>
 	);
-}
+};
 
 export default App;
