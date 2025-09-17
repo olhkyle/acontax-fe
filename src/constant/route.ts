a2f1e910e3ae0badbfbfefa749d0a0ab02990892
olhkyle <kylekwon.dev@gmail.com>
2025-09-17 17:02:09 +0900

    constant : add 5 routes
    - LOGIN, SIGNUP, FORGOT_PASSWORD, SETTINGS, SETTINGS_MYACCOUNT_PROFILE

--- src/constant/route.ts
+++ src/constant/route.ts
@@ -1,8 +1,13 @@
 const route = {
 	HOME: '/',
+	LOGIN: '/auth/login',
+	SIGNUP: '/auth/signup',
+	FORGOT_PASSWORD: '/auth/forgot-password',
 	WRITE: '/new',
 	STATEMENTS: '/statements',
 	GET_DATA_FROM_EXCEL: '/from-excel',
+	SETTINGS: '/settings',
+	SETTINGS_MYACCOUNT_PROFILE: '/settings/myaccount/profile',
 };
 
 export default route;
