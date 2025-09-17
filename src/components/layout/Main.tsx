34c9bb6ce472bddcfad91babaf638336c0b13d63
olhkyle <kylekwon.dev@gmail.com>
2025-09-17 17:02:40 +0900

    style : update `Main` component style

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,7 +1,7 @@
 import { type ReactNode } from 'react';
 
 const Main = ({ children }: { children: ReactNode }) => {
-	return <main className="p-4">{children}</main>;
+	return <main className="relative flex-1 bg-white p-4">{children}</main>;
 };
 
 export default Main;
