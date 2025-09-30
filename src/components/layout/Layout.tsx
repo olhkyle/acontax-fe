d1508f05cf41be65c9d749e1fc4cef793bcdbd9c
olhkyle <itskkyle@gmail.com>
2025-09-30 14:13:13 +0900

    layout : add `Outlet` to show different UI on each pages

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,12 +1,14 @@
+import { Outlet } from 'react-router';
 import { Aside, Main, Nav } from '.';
 
 const Layout = () => {
 	return (
-		<div className="flex min-h-screen flex-col">
+		<div className="flex flex-col min-h-screen">
 			<div className="flex flex-1">
 				<Aside />
 				<Main>
 					<Nav />
+					<Outlet />
 				</Main>
 			</div>
 		</div>
